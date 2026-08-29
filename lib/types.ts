17d10c84b11f339996817f4cc7a3e3527721f4de
caseysmeekes <casey.smeekes@gmail.com>
2026-08-29 20:59:36 +1200

    Add scheduler domain types

---
+++ lib/types.ts
@@ -0,0 +1,34 @@
+export type Position = 'Plate' | 'Base'
+
+export type Game = {
+  id: string
+  number: number
+  date: string
+  start: string
+  end: string
+  field: string
+  teams: string
+  division: string
+  positions: Position[]
+}
+
+export type Umpire = {
+  id: string
+  name: string
+  availability: string
+  maxGames: number
+  experience: 'International' | 'National' | 'Regional' | 'Developing'
+}
+
+export type Assignment = {
+  gameId: string
+  umpireId: string
+  position: Position
+}
+
+export type Violation = {
+  umpireId: string
+  rule: 'MAX_GAMES' | 'BACK_TO_BACK' | 'PLATE_BREAK'
+  message: string
+  gameId?: string
+}
