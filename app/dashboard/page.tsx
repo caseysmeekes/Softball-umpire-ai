e0b2dbd235611ed40c846215dc5068bde42edf2f
caseysmeekes <casey.smeekes@gmail.com>
2026-09-01 12:42:39 +1200

    Add Phase 7C start new tournament action

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import MultiDayDashboard from './MultiDayDashboard'
 import TournamentCompletionBar from './TournamentCompletionBar'
+import StartNewTournament from './StartNewTournament'
 
 export default function DashboardPage() {
   return (
     <>
       <TournamentCompletionBar />
+      <div className="start-new-wrap">
+        <StartNewTournament />
+      </div>
       <MultiDayDashboard />
     </>
   )
